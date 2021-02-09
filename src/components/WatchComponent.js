4c433f96e9cd9e436a1840f4296f4ea7e51661dc
D Pringle <n305oul93@gmail.com>
2021-02-09 10:43:01 -0600

    Setups Watch component

--- src/components/WatchComponent.js
+++ src/components/WatchComponent.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import { SLIDERIMAGES } from '../shared/sliderImages';
+
+const sliderImages = SLIDERIMAGES;
 
 function Watch() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === sliderImages.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === 0 ? sliderImages.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = newIndex => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = sliderImages.map(sliderImage => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={sliderImage.id}
+      >
+        <img src={sliderImage.image} alt={sliderImage.altText} />
+        <CarouselCaption
+          captionText={sliderImage.captionText}
+          captionHeader={sliderImage.captionHeader}
+        />
+      </CarouselItem>
+    );
+  });
+
   return (
-    <div>
-      <h4>Watch Page</h4>
-    </div>
+    <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+      <CarouselIndicators
+        items={sliderImages}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction='prev'
+        directionText='Previous'
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction='next'
+        directionText='Next'
+        onClickHandler={next}
+      />
+    </Carousel>
   );
 }
 
---
+++ src/shared/sliderImages.js
@@ -0,0 +1,26 @@
+export const SLIDERIMAGES = [
+  {
+    id: 0,
+    name: 'Image 1',
+    image: '../assets/images/slider/gateway.jpg',
+    altText: 'Slide 1',
+    captionText: 'Slide 1 Text',
+    captionHeader: 'Slide 1 Header'
+  },
+  {
+    id: 1,
+    name: 'Image 2',
+    image: '../assets/images/slider/Line-21-promo.jpg',
+    altText: 'Slide 2',
+    captionText: 'Slide 2 Text',
+    captionHeader: 'Slide 2 Header'
+  },
+  {
+    id: 2,
+    name: 'Image 3',
+    image: '../assets/images/slider/ratnik.jpg',
+    altText: 'Slide 3',
+    captionText: 'Slide 3 Text',
+    captionHeader: 'Slide 3 Header'
+  }
+];
