eede7ca4949e0ed4826ce580009e108f04ac9629
D Pringle <n305oul93@gmail.com>
2021-02-17 10:32:40 -0600

    Adds fade-in effect for about page

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,9 +1,112 @@
 import React from 'react';
+import '../css/About.css';
+import FadeIn from 'react-fade-in';
 
 function About() {
   return (
-    <div>
-      <h4>About Us</h4>
+    <div className='about-us'>
+      <div className='container'>
+        <FadeIn delay='1000' transitionDuration='5000'>
+          <h1>The Future Is Ours</h1>
+          <p>
+            INDY-FI is brought to you by InAvision Media, a studio that empowers
+            creators to take risks and experiment relentlessly in the pursuit of
+            novel stories and formats.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+            quasi ad eaque, sapiente dolorum molestiae vitae provident fuga at
+            vel distinctio rerum esse blanditiis, reprehenderit nisi quae
+            explicabo accusantium ratione perferendis quas dolore. Hic est quam
+            repellendus soluta nobis esse dicta aliquid enim, distinctio quaerat
+            voluptate dolorum dolorem laborum! Ullam, optio adipisci nemo ab
+            magnam iusto tempora explicabo fugiat quidem vitae dolorem
+            repellendus voluptas, in, voluptatum laborum? Suscipit, explicabo
+            beatae. Iusto magnam soluta, adipisci voluptas quae voluptatibus
+            voluptate reprehenderit ipsum deleniti qui iste voluptates sunt
+            quisquam voluptatem ducimus mollitia corrupti consequatur minus.
+            Mollitia laborum itaque voluptas esse, obcaecati libero aperiam!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
+            nobis sit repellendus? Amet ipsa, magnam, nisi ullam quos quis
+            distinctio iure eius doloremque odit, porro in voluptate modi.
+            Recusandae amet veniam ab odit provident dolorem minus omnis tempora
+            deserunt! Delectus molestias eum aspernatur at quibusdam accusamus
+            beatae cumque doloremque ex provident sint sunt harum, odio
+            inventore a quis vel quam incidunt nobis expedita omnis
+            exercitationem aut eaque asperiores. Dolorum placeat tenetur minus
+            quia, quibusdam dolores repudiandae consequatur delectus mollitia
+            rem laboriosam voluptate magni quasi velit quae praesentium
+            blanditiis eos enim recusandae possimus hic quo cupiditate
+            perferendis nesciunt. Praesentium, modi dolorum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            molestiae maiores natus omnis ad, voluptatem recusandae ipsam fugiat
+            magnam tempora sunt corrupti, assumenda fugit, doloremque laboriosam
+            ea ut voluptate suscipit. Cum, porro odit? Corporis vitae sit modi
+            recusandae quos laboriosam, necessitatibus non iusto consequatur
+            unde perferendis id aut, excepturi nemo eius quas nostrum dolores
+            iure quo. Facere nam laudantium dolor voluptatibus praesentium
+            fugiat saepe, nihil placeat obcaecati inventore maiores rem,
+            blanditiis totam dolore in molestiae, nemo numquam? Culpa
+            voluptatibus voluptate voluptatem voluptas iure amet officiis ea
+            optio unde possimus suscipit facere voluptatum, velit alias, impedit
+            distinctio, quos perferendis reiciendis. Magni quia quaerat quam
+            tenetur ullam inventore eligendi nam voluptates ea expedita aut
+            architecto exercitationem voluptate ad earum possimus delectus
+            blanditiis, doloribus ipsum aliquid dolorum at debitis enim
+            explicabo! Vero enim odit earum repudiandae! Ratione non esse
+            veritatis accusantium unde vero corrupti cum totam, beatae error
+            voluptatum? Nam aspernatur repellendus, laudantium cumque id laborum
+            esse repellat, fugit saepe suscipit repudiandae sapiente quae
+            quaerat impedit eum, consequatur ad assumenda in. Perferendis
+            recusandae repellendus, quod officia eum repudiandae voluptas
+            cupiditate, cumque facilis incidunt laboriosam vel inventore rerum
+            debitis. Fugiat obcaecati enim nesciunt unde esse est tempora
+            laudantium, incidunt, ex, atque ducimus rem aut!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio nam
+            dicta natus eaque assumenda non sapiente numquam, exercitationem
+            placeat laudantium quia voluptatem ad adipisci temporibus maxime
+            inventore iste facere a? Assumenda harum sint quasi dignissimos
+            accusantium nobis ipsa, quidem maxime beatae asperiores quos quas
+            iste corrupti, vitae cumque. Distinctio pariatur totam beatae,
+            quaerat veniam quos ipsam a sapiente? Magnam nostrum ut consectetur
+            accusamus delectus mollitia distinctio! Perferendis eveniet corrupti
+            minus quas inventore ad delectus culpa asperiores sequi sapiente
+            iste et tempore debitis ex, quo iure. Deleniti placeat mollitia
+            aliquam vitae neque rem culpa quod quia est quaerat consequatur
+            adipisci quidem officiis numquam, fugiat assumenda id laborum
+            suscipit sapiente, maxime iste? Velit natus quasi suscipit, vel
+            ipsam reiciendis placeat optio quibusdam eveniet amet quam,
+            consequuntur recusandae inventore. Assumenda soluta in officia unde
+            dicta! Cupiditate ullam blanditiis mollitia recusandae velit
+            repudiandae sunt eligendi facilis culpa exercitationem veritatis
+            voluptate inventore iure magni placeat dolor deleniti, laborum neque
+            vel sed quam ipsum incidunt laboriosam. Enim cupiditate dolore dicta
+            maiores, minus molestiae eius commodi facere, numquam iusto et
+            architecto aliquam magni illo aspernatur a hic quis cum! Modi
+            aliquid inventore nulla. Tenetur, necessitatibus esse eveniet, ab
+            molestias dolorum reprehenderit porro a eaque officiis nostrum.
+            Optio expedita doloribus quisquam recusandae qui voluptas laborum
+            tempore adipisci architecto in! Sequi dolorum corrupti deserunt et,
+            sed inventore officiis velit ex provident nobis quis eum, harum
+            exercitationem, consequuntur esse facilis fugit rerum nostrum?
+            Blanditiis, hic doloremque. Maiores fuga quo doloremque sed
+            blanditiis quia animi quaerat possimus sit repudiandae, cum,
+            veritatis voluptate. Reiciendis fuga iusto eligendi placeat,
+            distinctio, perferendis cumque rerum repudiandae molestiae ex
+            similique possimus consequuntur! Laudantium maiores in, amet sit
+            sapiente voluptate velit numquam iste suscipit neque corrupti!
+            Repellendus voluptas quod reiciendis expedita, quae, consequuntur
+            cumque unde maxime repudiandae tempora architecto libero natus non
+            obcaecati praesentium possimus pariatur iste.
+          </p>
+        </FadeIn>
+      </div>
     </div>
   );
 }
